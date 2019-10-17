a415a41288023e9cfa8fb8a3e55995bd6c20b22a
Rene <rene_perez@outlook.it>
2019-10-17 08:32:21 -0400

    Base de Datos TABLAS(Persona,Usuario)

--- Proyecto/database/database.sql
+++ Proyecto/database/database.sql
@@ -1,15 +1,29 @@
-CREATE DATABASE cuidaTuMascota_db
+CREATE DATABASE cuidaTuMascota_db;
 
-USE cuidaTuMascota_db
+USE cuidaTuMascota_db;
+
+CREATE TABLE Usuario(
+    idUsuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Email VARCHAR(40),
+    UserName VARCHAR(15),
+    Pass VARCHAR(25),
+    Estado BIT,
+    TipoUsuario VARCHAR(45)
+);
+DESCRIBE Usuario;
 
 CREATE TABLE Persona(
     idPersona INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombres VARCHAR(45),
-    primerApellido VARCHAR(45),
-    segundoApellido VARCHAR(45),
-    rol VARCHAR(45),
-    estado BIT,
-    idHORARIO INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    Nombres VARCHAR(60),
+    PrimerApellido VARCHAR(45),
+    SegundoApellido VARCHAR(45),
+    Estado BIT,
+    idHorario INT,
+    idUsuario INT,
+    Created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
 );
 DESCRIBE Persona;
+
+
+
