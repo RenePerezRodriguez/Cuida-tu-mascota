3d8b169ba3a5c8fa4f764af9fd765af610911c3b
Rene <rene_perez@outlook.it>
2019-12-03 14:21:03 -0400

    Control de sessiones

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private afsAuth: AngularFireAuth) { }
+  public app_name = 'Asistencias QR';
+  public isLogged = false;
+  public isAdmin: any = null;
+  public userUid: string = null;
 
   ngOnInit() {
+    this.getCurrentUser();
+
+  }
+
+  getCurrentUser() {
+    this.authService.isAuth().subscribe(auth => {
+      if (auth) {
+        console.log('user logged');
+        this.isLogged = true;
+        this.userUid = auth.uid;
+        this.authService.isUserAdmin(this.userUid).subscribe(userRole => {
+          this.isAdmin = Object.assign({}, userRole.roles).hasOwnProperty('admin');
+        });
+      } else {
+        console.log('NOT user logged');
+        this.isLogged = false;
+      }
+    });
+  }
+
+  onLogout() {
+    this.afsAuth.auth.signOut();
   }
 
 }
