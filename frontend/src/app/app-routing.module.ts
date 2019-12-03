67b9da9dc5a6314568d8c18d4820ede6f8c4ed44
Rene <rene_perez@outlook.it>
2019-12-03 02:23:38 -0400

    correctiones

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,15 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:'',
-    redirectTo:'/home',
-    pathMatch:'full'
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
   },
   { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
   { path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) },
-  { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('./components/auth/register/register.module').then(m => m.RegisterModule) }
-];
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,44 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 
-/* FIREBASE */
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireStorageModule, StorageBucket } from '@angular/fire/storage';
-import {AngularFireModule} from '@angular/fire';
-import {environment} from '../environments/environment';
-import { ListPetsComponent } from './components/list-pets/list-pets.component';
-import { ModalLoginComponent } from './components/modals/modal-login/modal-login.component';
-import { ModalRegisterComponent } from './components/modals/modal-register/modal-register.component';
-import { ProfileComponent } from './components/users/profile/profile.component';
-import { Page404Component } from './components/pages/page404/page404.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-<<<<<<< HEAD
-    ListPetsComponent,
-    ModalLoginComponent,
-    ModalRegisterComponent,
-    ProfileComponent,
-    Page404Component
-=======
-    NavigationComponent,
-    FooterComponent
->>>>>>> master
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
+    AngularFireDatabaseModule,
     AngularFireStorageModule,
-    AppRoutingModule
   ],
-  providers: [
-  {provide: StorageBucket, useValue: 'gs://cuidatumascota-3fd63.appspot.com'}
-  ],
+  providers: [AngularFireAuth, AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
