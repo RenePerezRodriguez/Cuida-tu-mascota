7b36a4a0f4a11d448f9632165cd06d325654c06c
vhugosm <hsullmdz@gmail.com>
2019-12-03 02:12:10 -0400

    CREACION DE NAVEGACION

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
   { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
   { path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) },
   { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) },
--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -3,7 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css',
+  '../../../Template/doglife/fonts/icomoon/style.css',
+  '../../../Template/doglife/css/bootstrap.min.css',
+  '../../../Template/doglife/css/jquery-ui.css',
+  '../../../Template/doglife/css/owl.carousel.min.css',
+  '../../../Template/doglife/css/owl.theme.default.min.css',
+  '../../../Template/doglife/css/jquery.fancybox.min.css',
+  '../../../Template/doglife/css/bootstrap-datepicker.css',
+  '../../../Template/doglife/fonts/flaticon/font/flaticon.css',
+  '../../../Template/doglife/css/aos.css',
+  '../../../Template/doglife/css/style.css']
 })
 export class HomeComponent implements OnInit {
 
